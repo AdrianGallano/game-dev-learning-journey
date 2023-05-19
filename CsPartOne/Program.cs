e9c50e0a1805c450ae3e2c1319eaac40dfe44e33
Adrian Gallano <adriangallanoengr@gmail.com>
2023-05-19 13:34:54 +0800

    Add String basics

---
+++ CsPartOne/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+// Program 1
+Console.WriteLine("Name: Adrian Cris Gallano");
+
+// Program 2
+Console.WriteLine("Name: " + "Adrian Cris Gallano");
+
+// Program 3
+string name = "Adrian Gallano";
+Console.WriteLine("Name: " + name);
+
+// Program 4 - String Interpolation
+Console.WriteLine($"Name: {name}"); 
+
+// Program 5 - String Interpolation part 2
+string megaIdok = "oyam";
+Console.WriteLine($"My name is {name} and my bestfriend is {megaIdok}");
+
+// Program 6 - the Length Property
+Console.WriteLine($"My name is {name} and it has {name.Length} letters");
