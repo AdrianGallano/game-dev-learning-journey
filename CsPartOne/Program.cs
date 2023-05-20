334380c34ddc105499e1b1871c014250d20d4f57
Adrian Gallano <adriangallanoengr@gmail.com>
2023-05-20 18:49:52 +0800

    Add authentication

--- CsPartOne/Program.cs
+++ CsPartOne/Program.cs
@@ -19,3 +19,34 @@ Console.WriteLine($"My name is {name} and my bestfriend is {megaIdok}");
 
 // Program 6 - the Length Property
 Console.WriteLine($"My name is {name} and it has {name.Length} letters");
+
+// Program 7 - Trimming
+string sampleSentence = "    A Quick Fox is here    ";
+Console.WriteLine($"[{sampleSentence.TrimStart()}]");
+Console.WriteLine($"[{sampleSentence.TrimEnd()}]");
+Console.WriteLine($"[{sampleSentence.Trim()}]");
+
+// Program 8 - Replace
+
+string sentence = "I love apple because apple is delicious";
+string newSentence =  sentence.Replace("apple", "orange");
+Console.WriteLine(newSentence);
+
+// Program 9 - Upper and Lower
+string newName = "Adrian Gallano";
+Console.WriteLine(newName.ToLower());
+Console.WriteLine(newName.ToUpper());
+Console.WriteLine(newName);
+
+// Program 10 = Contains
+Console.WriteLine(sentence.Contains("love"));
+Console.WriteLine(sentence.Contains("hate"));
+
+// Program 11 = Starts With
+Console.WriteLine(sentence.StartsWith("I love"));
+Console.WriteLine(sentence.StartsWith("love"));
+
+// Program 12 = EndsWith
+Console.WriteLine(sentence.EndsWith("delicious"));
+Console.WriteLine(sentence.EndsWith("is"));
+Console.Beep();
---
+++ CsPartTwo/Program.cs
@@ -0,0 +1,90 @@
+﻿Console.ForegroundColor = ConsoleColor.Green;
+
+bool run = true;
+string usernameInput = null;
+string passwordInput = null;
+while(run){
+
+    Console.WriteLine("REGISTER PANEL");
+
+    Console.Write("Enter your username: ");
+    usernameInput = Console.ReadLine();
+
+    if(usernameInput.Length < 8){
+      Console.WriteLine("Username length is less than 8, please try again.");
+      Console.WriteLine("Press any key to continue.");
+      Console.ReadKey();
+      Console.Clear();
+      continue;
+    }else{
+      break;
+    }
+
+}
+
+while(run){
+    Console.Write("Enter your password: ");
+    passwordInput = Console.ReadLine();
+
+    if(passwordInput.Length < 8){
+      Console.WriteLine("Password length is less than 8, please try again.");
+      Console.WriteLine("Press any key to continue.");
+      Console.ReadKey();
+      Console.Clear();
+      continue;
+    }else{
+      break;
+    }
+}
+Console.Clear();
+Console.Beep();
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("Succesfully Registered!");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+
+Console.ReadLine();
+
+
+string username;
+string password;
+
+while(run){
+
+    Console.WriteLine("LOGIN PANEL");
+
+    Console.Write("Enter your username: ");
+    username = Console.ReadLine();
+
+    if(username != usernameInput){
+      Console.WriteLine("username incorrect");
+      Console.WriteLine("Press any key to continue.");
+      Console.ReadKey();
+      Console.Clear();
+      continue;
+    }else{
+      break;
+    }
+}
+
+while(run){
+    Console.WriteLine("LOGIN PANEL");
+
+    Console.Write("Enter your password: ");
+    password = Console.ReadLine();
+
+    if(password != passwordInput){
+      Console.WriteLine("password incorrect");
+      Console.WriteLine("Press any key to continue.");
+      Console.ReadKey();
+      Console.Clear();
+      continue;
+    }else{
+      break;
+    }
+}
+
+Console.WriteLine("CONGRATULATIONS YOU HAVE LOGGED IN SUCCESSFULLY.");
+Console.WriteLine("Press any key to continue.");
+Console.ReadKey();
