0d0f8c09b7090bd598a9e128c8e751109043b12e
Adrian Gallano <adriangallanoengr@gmail.com>
2023-06-08 23:37:58 +0800

    Add version docs

--- Humanize/Program.cs
+++ Humanize/Program.cs
@@ -10,5 +10,21 @@
       Console.WriteLine("\nHumanize Dates: ");
       IDescriptive.HumanizeDate();
     }
+
+    /* 
+    if 1.0 inclusive
+    if (1.0,) 1.1 up
+    if [1.0,] 1.0 up
+    if (,1.0] 1.0 >= x
+    if (,1.0) 1.0 > x
+    if [1,3] 1.0 to 3.0
+    if (1,3) 2.x.x
+    if [1.0, 3.0) 1.0 >= x > 3.0
+    if (1.0) invalid
+
+    x.0.0 major update
+    0.x.0 minor update
+    0.0.x patch
+     */
   }
 }
